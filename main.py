c75d354a8e98068f27ea21bbe785b1b8c06199c9
j35393lo <louie.obrien@student.manchester.ac.uk>
2025-02-18 12:51:38 +0000

    saved the model

--- main.py
+++ main.py
@@ -95,16 +95,16 @@ def test(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
+# training
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
 
-# epochs = 10
-# for t in range(epochs):
-#     print(f"Epoch {t+1}\n-------------------------------")
-#     train(train_dataloader, model, loss_fn, optimizer)
-#     test(test_dataloader, model, loss_fn)
-# print("Done!")
-# 
-# torch.save(model.state_dict(), "model.pth")
-# print("Saved PyTorch Model State to model.pth")
+torch.save(model.state_dict(), "model.pth")
+print("Saved PyTorch Model State to model.pth")
 
 model = NeuralNetwork().to(device)
 model.load_state_dict(torch.load("model.pth", weights_only=True))
